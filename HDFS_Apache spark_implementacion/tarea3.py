c041a168149fe94f297812b0f3e93327604e5b21
pipe2015 <afelipe94@hotmail.com>
2024-10-23 21:44:30 -0500

    terminando las consultas tarea3.py en el server

--- HDFS_Apache spark_implementacion/tarea3.py
+++ HDFS_Apache spark_implementacion/tarea3.py
@@ -1,5 +1,7 @@
 #Importamos librerias necesarias
 from pyspark.sql import SparkSession, functions as F
+#from pyspark.sql.functions import col, avg, sum, count as _sum
+from pyspark.sql.functions import col, avg, sum, count, max, min, year
 
 """
 docs: https://jackyfu1995.medium.com/data-engineering-introduction-and-implementation-of-spark-python-7391c8da2e2e
@@ -35,3 +37,70 @@ total_records = df.count()
 print(f"\nTotal de registros: {total_records}")
 
 
+# 1. Realizamos operaciones de limpieza
+# Verificar si hay valores nulos
+print("Valores nulos en cada columna:")
+df.select([col(c).isNull().cast("int").alias(c) for c in df.columns]).show()
+
+# en esat primera parte estoy usando el objeto dataFrame
+
+# Eliminar filas con valores nulos
+clear_df = df.na.drop()
+
+# 2. Transformaciones
+# Convertir 'time_spent_seconds' de segundos a minutos usando la funcion map que pasa por parametro cada dato y lo
+# retorno con la transformacion
+clear_df = clear_df.withColumn("time_spent_minutes", col("time_spent_seconds") / 60)
+
+# Analisis exploratorio de datos (EDA)
+# Numero total de usuarios
+total_users = clear_df.select("user_id").distinct().count()
+print(f"Numero total de usuarios: {total_users}")
+
+# Sesiones por dispositivo
+sessions_by_device = clear_df.groupBy("device_type").count().orderBy("count", ascending=False)
+print("Sesiones por dispositivo:")
+sessions_by_device.show()
+
+# Numero total de cliks por usuario
+clicks_per_user = clear_df.groupBy("user_id").agg(sum("clicks").alias("total_clicks"))
+print("\nNumero total de cliks por usuario:")
+clicks_per_user.show()
+
+# Numero total de clikss por artículo
+clicks_by_article = clear_df.groupBy("article_name").agg(sum("clicks").alias("total_clicks")).orderBy("total_clicks", ascending=False)
+print("Total de clicks por articulo:")
+clicks_by_article.show()
+
+# Tiempo promedio que los usuarios pasan en las páginas
+average_time_spent = clear_df.groupBy("user_id").agg(avg("time_spent_seconds").alias("average_time_spent"))
+print("\nTiempo promedio que los usuarios pasan en las paginas (en segundos):")
+average_time_spent.show()
+
+# Videos vistos por categoria
+video_views_by_category = clear_df.groupBy("article_category").agg(sum("video_views").alias("total_video_views")).orderBy("total_video_views", ascending=False)
+print("Videos vistos por categoria:")
+video_views_by_category.show()
+
+# Numero de articulos leídos por categoria
+articles_per_category = clear_df.groupBy("article_category").agg(count("article_name").alias("articles_read"))
+print("\nNumero de articulos leidos por categoria:")
+articles_per_category.show()
+
+# Analisis de la ubicacion geografica
+geo_location_counts = clear_df.groupBy("Geo_Location").count().orderBy("count", ascending=False)
+print("Numero de interacciones por ubicación geografica:")
+geo_location_counts.show()
+
+# Filtrar los datos para el año 2024
+df_2024 = clear_df.filter(year(col("timestamp")) == 2024)
+
+# Obtener la primera y última interacción por usuario en 2024
+user_interactions_2024 = df_2024.groupBy("user_id").agg(
+    min("timestamp").alias("Fecha_Inicio"),
+    max("timestamp").alias("Fecha_Final")
+).orderBy(col("user_id").asc())
+
+# Mostrar el resultado
+print("Fecha de inicio y Final de cada usuario que interactuo con la pagina en el 2024")
+user_interactions_2024.show(truncate=False)
